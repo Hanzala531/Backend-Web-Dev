af2fe51e5489f13fc026c8e5560ee17a6ed7c221
Hanzala531 <165744107+Hanzala531@users.noreply.github.com>
2024-09-01 20:39:55 +0500

    commit

--- video 88/Express.js
+++ video 88/Express.js
@@ -2,8 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public')) ;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Home page</h1>')
+})
+app.get('/Resources', (req, res) => {
+  res.send('<h1>Resources Page</h1>')
+})
+app.get('/Contact', (req, res) => {
+  res.send('<h1>Comtact page</h1>')
+})
+app.get('/About', (req, res) => {
+  res.send('<h1>About page</h1>')
+})
+app.get('/Blog', (req, res) => {
+  res.send('<h1>Blog page</h1>')
+})
+// app.get('/Blog/intro-to-js', (req, res) => {
+//   //logic to be written to ftetch blog fropm the database
+//   res.send('<h1>Introduction to javascript</h1>')
+// })
+// app.get('/Blog/intro-to-Python', (req, res) => {
+//   //logic to be written to ftetch blog fropm the database
+//   res.send('<h1>Introduction to Python</h1>')
+// })
+
+app.get('/Blog/:slug', (req, res) => {
+  //logic to be written to ftetch blog fropm the database\
+  console.log(req.params);
+  console.log(req.query);
+  
+  res.send(`<h1>hi this is ${req.params.slug}</h1>`)
 })
 
 app.listen(port, () => {
